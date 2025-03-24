d8dbc0a01877a23aad4794c6a9613aa0136a5430
Terol-Chakma <chakmaterol1@gmail.com>
2025-03-24 21:50:54 +0530

    Delete string_reversal.py

--- string_reversal.py
+++
@@ -1,9 +0,0 @@
-
-#Task 1 : string reversal.
-def reverse_string():
-    str = input("Enter a string to reverse: ")
-    if not str:
-        return "Sorry!!! there is no input. please enter a string."
-    return str[::-1]
-
-print("The reversed string is: ", reverse_string())
